68289a25b7f1cbecba186d966e96612e6cf7ad1f
Igor Starobor <igorstarobor666@gmail.com>
2018-09-21 07:01:16 +0300

    init

---
+++ src/sample/Constants.java
@@ -0,0 +1,25 @@
+package sample;
+
+public interface Constants {
+    String AUTHORIZATION_URL = "https://www.instagram.com/accounts/login/?source=auth_switcher";
+
+    String OUTPUT_FOLDER = "D://VideoDownload/";
+
+    String USER_NAME = "username";
+    String PASSWORD = "pass";
+
+    String STORY_URL = "https://www.instagram.com/stories/";
+    String MAIN_URL = "https://www.instagram.com";
+
+    String[] USERS_TARGET = {"tazia.diana", "sirotkin.dima"};
+
+    String IS_VIDEO_TOKEN = "video class=\"y-yJ5  OFkrO \"";
+    String IS_PHOTO_TOKEN = "img class=\"y-yJ5 _7NpAS ";
+
+    String NEXT_STORY_BUTTON = "ow3u_";
+
+    String VIDEO_FORMAT = ".mp4";
+    String PHOTO_FORMAT = ".jpg";
+
+    int TIME_TO_LOAD = 1500;
+}
